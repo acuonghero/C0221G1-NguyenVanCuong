96dfc712f22f2f386a000297b3122e1b08fa411f
acuonghero <acuonghero@gmail.com>
2021-07-27 21:18:05 +0700

    up

---
+++ Module4/bai_thi/src/main/java/com/codegym/controller/test.java
@@ -0,0 +1,17 @@
+package com.codegym.controller;
+
+public class test {
+    public static void main(String[] args) {
+
+    }
+    public  static String[] find(String[] arr){
+        String [] test=[arr.length];
+        for(int i = 0 ; i< arr.length; i++){
+            for (int j = 0; j< test.length;j++){
+                if(!arr[i].contains(test[j])){
+
+                }
+            }
+        }
+    }
+}
---
+++ Module5/06_angular_form/exercises/src/app/register/register.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    country: new FormControl(''),
+    phone: new FormControl('')
+  });
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+}
